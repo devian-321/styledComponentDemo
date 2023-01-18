9f49be1a522ac65e5a7bb8f012a15c09ce95a754
Atharva Tiwari <atharvatiwari@Atharvas-MacBook-Air.local>
2023-01-19 03:32:45 +0530

    changed form to functional based components

--- src/components/Enter Detail/index.js
+++ src/components/Enter Detail/index.js
@@ -1,54 +1,93 @@
-import React,{Component} from "react";
+import React,{useState} from "react";
 import {H1,Label,InputChallan,Captcha,SubmitButton} from './form.styles'
 
 
-class Form extends Component {
-    constructor(props){
-        super(props)
+function Form() {
+    const [inputField,setInputVehicle]=useState({
+        vehicleNumber: ''
+    })
 
-        this.state = {
-            vehicleNumber: ''
-        }
-
-
+    const handleChallanChange = (event) =>{
+        setInputVehicle({[event.target.vehicleNumber]: event.target.value})
     }
-
-    handleChallanChange = (event)=>{
-        console.log(event.target.value);
-        this.setState({
-            vehicleNumber: event.target.value
-        })
-
+    const submition = ()=>{
+        alert(inputField.vehicleNumber)
     }
-
-
-
-render() {
     return(
-        <form>
-            <div>
-                <H1>E-challan Payment</H1>
+                <form>
+                     <div>
+                         <H1>E-challan Payment</H1>
+        
+        
+                         <InputChallan type="text" 
+                                        placeholder="Enter your Challan Number/Vehicle Nmber"
+                                        value={inputField.vehicleNumber} 
+                                         onChange={handleChallanChange} />
+        
+                         <Label>
+                             <div class="input-container">
+                                 <input type="checkbox" class="checkbox" id="check" />
+        
+                                 <span class="checkbox-text">I'm not a robot</span>
+                                
+                             </div>
+                         </Label>
+        
+                         <SubmitButton onClick={submition}>SUBMIT</SubmitButton>
+        
+                     </div>
+                 </form>
+    )}
+
+export default Form;
+
+// class Form extends Component {
+//     constructor(props){
+//         super(props)
+
+//         this.state = {
+//             vehicleNumber: ''
+//         }
 
 
-                <InputChallan type="text"  value={this.state.vehicleNumber} 
+//     }
+
+//     handleChallanChange = (event)=>{
+//         console.log(event.target.value);
+//         this.setState({
+//             vehicleNumber: event.target.value
+//         })
+
+//     }
+
+
+
+// render() {
+//     return(
+//         <form>
+//             <div>
+//                 <H1>E-challan Payment</H1>
+
+
+//                 <InputChallan type="text"  value={this.state.vehicleNumber} 
                 
-                                            onChange={this.handleChallanChange} />
+//                                             onChange={this.handleChallanChange} />
 
-                <Label>
-                    <div class="input-container">
-                        <input type="checkbox" class="checkbox" id="check" />
+//                 <Label>
+//                     <div class="input-container">
+//                         <input type="checkbox" class="checkbox" id="check" />
 
-                        <span class="checkbox-text">I'm not a robot</span>
+//                         <span class="checkbox-text">I'm not a robot</span>
                         
-                    </div>
-                </Label>
+//                     </div>
+//                 </Label>
 
-                <SubmitButton>Submit</SubmitButton>
+//                 <SubmitButton>Submit</SubmitButton>
 
-            </div>
-        </form>
-    )
-}
-}
+//             </div>
+//         </form>
+//     )
+// }
+// }
 
-export default Form 
+// export default Form 
