8e8ab69e61ee7a2bc464df5d007321316bc9cefb
Atharva Tiwari <atharvatiwari@Atharvas-MacBook-Air.local>
2023-01-23 16:28:41 +0530

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
-import Form from './components/Enter Detail'
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Form from './components/Enter Detail';
 import UnpaidChallan from './components/Paid Challan';
 
 function App() {
   return (
     <div className="App">
-      {/* <Form>Form Component</Form> */}
-    <UnpaidChallan> unpaidChallan</UnpaidChallan>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Form />} />
+          <Route exact path='/unpaidChallan' element={<UnpaidChallan />} />
+        </Routes>
+      </BrowserRouter>
+       {/* <Form>Form Component</Form>
+    <UnpaidChallan> unpaidChallan</UnpaidChallan> */}
     </div>
     
   
